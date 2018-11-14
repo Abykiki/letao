5269ab55609c7a9e38d9aabb18b51869d1e27021
Abykiki <ww.Sun@qq.com>
2018-11-14 11:33:22 +0800

    使用表单校验插件完成基本表单校验

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,22 +1,55 @@
-// 1.表单校验
 
 $(function(){
+  // 1.表单校验
   // 要求
   //(1) 用户名不能为空 ,长度为2-6位
   //(2)密码不能为空,长度为6-12位
-
-
-  // 配置校验图标
-
+ $('#form').bootstrapValidator({
+   // 配置校验图标
+   feedbackIcons: {
+    //  校验成功
+    valid: 'glyphicon glyphicon-ok',
+    // 校验失败
+    invalid: 'glyphicon glyphicon-remove',
+    // 校验中
+    validating: 'glyphicon glyphicon-refresh'
+  },
   
-  // 配置校验字段(现在input中配置name)
+   // 配置校验字段(先在input中配置name)
+  fields:{
+    username:{
 
-
-  // 进行多个规则配置
+      // 进行多个规则配置
+      validators:{
+        // 非空校验
+        notEmpty:{
+          // 校验提示
+          message:"用户名不能为空"
+        },
+        // 长度校验
+        stringLength:{
+          min:2,
+          max:6,
+          message:"用户名长度必须为2-6位"
+        }
+      }
+     
+    },
+    password:{
+      validators:{
+        notEmpty:{
+          message:"密码不能为空"
+        },
+        stringLength:{
+          min:6,
+          max:12,
+          message:"密码长度必须为6-12位"
+        }
+      }
+    },
+  }
   
-  // 非空校验
+  
+ })
 
-  // 校验提示
-
-  // 长度校验
 })
